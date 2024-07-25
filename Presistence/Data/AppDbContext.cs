e61dc02d77d9d21ebc35e6113d7466215eb0a374
Eyas Wannous <eyaswannous@gmail.com>
2024-07-25 12:31:09 +0300

    Add "AppDbContext" and "UnitOfWork"

---
+++ Presistence/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VideoToPostGenerationAPI.Domain.Entities;
+
+namespace VideoToPostGenerationAPI.Presistence.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<User, IdentityRole<int>, int>(options)
+{
+    public DbSet<Video> Videos { get; set; }
+    public DbSet<Audio> Audios { get; set; }
+    public DbSet<GeneralPost> GeneralPosts { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Image> Images { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    if (!optionsBuilder.IsConfigured)
+    //    {
+    //        optionsBuilder.UseSqlServer("");
+    //    }
+    //}
+}
---
+++ Presistence/Data/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using VideoToPostGenerationAPI.Domain.Abstractions;
+using VideoToPostGenerationAPI.Domain.Abstractions.IRepositories;
+using VideoToPostGenerationAPI.Presistence.Repositories;
+
+namespace VideoToPostGenerationAPI.Presistence.Data;
+
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    private bool _disposed = false;
+    private readonly AppDbContext _context = context;
+    public IUserRepository Users { get; } = new UserRepository(context);
+
+    public IVideoRepository Videos { get; }  = new VideoRepository(context);
+
+    public IAudioRepository Audios { get; } = new AudioRepository(context);
+
+    public IGeneralPostRepository GeneralPosts { get; } = new GeneralPostRepository(context);
+
+    public IPostRepository Posts { get; } = new PostRepository(context);
+
+    public IImageRepository Images { get; } = new ImageRepository(context);
+
+    public async Task<int> CompleteAsync() => await _context.SaveChangesAsync();
+
+    // disposing : true (dispose managed + unmanaged)
+    // disposing : false (dispose unmanaged + large fields)
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed)
+            return;
+
+        // Dispose Logic
+        if (disposing)
+        {
+            //Dispose Managed Resources
+            _context.Dispose();
+        }
+
+        _disposed = true;
+    }
+
+    public void Dispose()   
+    {
+        // Disopose is 100% calling
+        Dispose(true);
+
+        // notify garbage collector to not call destructor
+        GC.SuppressFinalize(this);
+    }
+
+    ~UnitOfWork()
+    {
+        Dispose(false);
+    }
+}
