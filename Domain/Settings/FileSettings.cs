f1f9c380510968506093d55ea4cddbf32c0524b7
Eyas Wannous <eyaswannous@gmail.com>
2024-07-25 12:39:19 +0300

    Add Settings

---
+++ Domain/Settings/FileSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace VideoToPostGenerationAPI.Domain.Settings;
+
+public static class FileSettings
+{
+    public const string WebRootPath = "C:\\Users\\eyasw\\Desktop\\VideoToPostGenerationAPI\\wwwroot\\";
+    public const string ImagesPath = "assets\\images\\"; // @"assets\images\"
+    public const string AudiosPath = "assets\\audios\\"; // @"assets\audios\"
+    public const string VideosPath = "assets\\videos\\"; // @"assets\videos\"
+    //public const string AllowedExtensions = ".jpg,.png,.jpeg,.pdf";
+    public const int MaxFileSizeInGB = 5;
+    public const long MaxFileSizeInBytes = (long) MaxFileSizeInGB * 1024 * 1024 * 1024;
+}
+
