9eb03ec4be5b0ad02983f39f1b14448cb513ddb4
Eyas Wannous <eyaswannous@gmail.com>
2024-07-25 12:28:44 +0300

    Add Abstraction of UnitOfWork

---
+++ Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using VideoToPostGenerationAPI.Domain.Abstractions.IRepositories;
+
+namespace VideoToPostGenerationAPI.Domain.Abstractions;
+
+public interface IUnitOfWork : IDisposable
+{
+    IUserRepository Users { get; }
+    IVideoRepository Videos { get; }
+    IAudioRepository Audios { get; }
+    IGeneralPostRepository GeneralPosts { get; }
+    IPostRepository Posts { get; }
+    IImageRepository Images { get; }
+    Task<int> CompleteAsync();
+}
