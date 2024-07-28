bbc50c4ae2c7b664f5a62c5855140d7f99dc5fd4
Eyas Wannous <eyaswannous@gmail.com>
2024-07-28 22:41:36 +0300

    Add YouTube Service for captions

---
+++ Domain/Abstractions/IServices/IYouTubeService.cs
@@ -0,0 +1,7 @@
+﻿
+namespace VideoToPostGenerationAPI.Domain.Abstractions.IServices;
+
+public interface IYouTubeService
+{
+    Task<string?> GetVideoCaptions(string videoURL, string languagePrefix);
+}
---
+++ Services/YouTubeService.cs
@@ -0,0 +1,20 @@
+﻿using VideoToPostGenerationAPI.Domain.Abstractions.IServices;
+using YoutubeExplode;
+
+namespace VideoToPostGenerationAPI.Services;
+
+public class YouTubeService : IYouTubeService
+{
+    private readonly YoutubeClient _youtubeClient = new();
+
+    public async Task<string?> GetVideoCaptions(string videoURL, string languagePrefix)
+    {
+        var trackManifest = await _youtubeClient.Videos.ClosedCaptions.GetManifestAsync(videoURL);
+        var trackInfo = trackManifest.TryGetByLanguage(languagePrefix); // Get English captions
+        if (trackInfo is null)
+            return null;
+        
+        var track = await _youtubeClient.Videos.ClosedCaptions.GetAsync(trackInfo);
+        return string.Join(" ", track.Captions.Select(caption => caption.Text));
+    }
+}
