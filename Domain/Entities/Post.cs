c099000ba2d01cd5f71c3cf88fb1b08c3112e49b
Eyas Wannous <eyaswannous@gmail.com>
2024-07-31 12:32:28 +0300

    Update post and change AudioId to VideoId

--- Domain/Entities/Post.cs
+++ Domain/Entities/Post.cs
@@ -7,8 +7,8 @@ public class Post : BaseEntitiy
 {
     public string Description { get; set; } = string.Empty;
     public string Platform { get; set; } = string.Empty;
-    public int AudioId { get; set; }
-    public Video Audio { get; set; }
+    public int VideoId { get; set; }
+    public Video Video { get; set; }
     public Header? Header { get; set; }
     public List<Image> Images { get; set; } = [];
 }
