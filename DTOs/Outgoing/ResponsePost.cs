06d0f0d98f936e0cf5e697d2d599b85730bea067
Eyas Wannous <eyaswannous@gmail.com>
2024-07-31 12:33:45 +0300

    Add "ResponsePost.cs" to DTOs

---
+++ DTOs/Outgoing/ResponsePost.cs
@@ -0,0 +1,13 @@
+﻿using VideoToPostGenerationAPI.Domain.Entities;
+
+namespace VideoToPostGenerationAPI.DTOs.Outgoing;
+
+public class ResponsePost
+{
+    public int Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Platform { get; set; } = string.Empty;
+    public Header? Header { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
