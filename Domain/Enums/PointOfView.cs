0016e9620cc4e325b540657f0f84bd79924314c2
Eyas Wannous <eyaswannous@gmail.com>
2024-08-30 14:40:51 +0300

    Add 'PointOfView' and 'PostFormat' Enums

---
+++ Domain/Enums/PointOfView.cs
@@ -0,0 +1,10 @@
+﻿namespace VideoToPostGenerationAPI.Domain.Enums;
+
+public enum PointOfView
+{
+    Auto = 1,
+    FirstPersonSingular,
+    FirstPersonPlural,
+    SecondPerson,
+    ThirdPerson,
+}
---
+++ Domain/Enums/PostFormat.cs
@@ -0,0 +1,13 @@
+﻿namespace VideoToPostGenerationAPI.Domain.Enums;
+
+public enum PostFormat
+{
+    Auto = 1,
+    Summary,
+    Promotional,
+    //Interview
+    //Listicle,
+    //ProductReview,
+    //NewsReport,
+    //Tutorial,
+}
