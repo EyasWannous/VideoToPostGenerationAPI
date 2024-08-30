7a4493c99e94f91f5cb1d6b7b719a7f3e6803c20
Eyas Wannous <eyaswannous@gmail.com>
2024-08-30 14:54:14 +0300

    Add Accept All Policies

---
+++ IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace VideoToPostGenerationAPI;
+
+public static class IServiceCollectionExtensions
+{
+    public static IServiceCollection AddCorsDevelopmentPolicy(this IServiceCollection services)
+    {
+        services.AddCors(opt => opt
+            .AddPolicy("Ahmad", policy => policy
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin()
+                )
+            );
+
+        return services;
+    }
+
+
+}
